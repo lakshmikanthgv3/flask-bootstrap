776041932e879d9a2283fd74800a7b3b65c86063
lakshmikanth <71622256+lakshmikanthgv3@users.noreply.github.com>
2025-11-05 19:23:49 +0530

    First commit

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM python:3.12
+
+WORKDIR /app
+
+COPY requirements.txt requirements.txt
+RUN pip install --no-cache-dir -r requirements.txt
+
+EXPOSE 5000
+
+ENV FLASK_APP=app.py
+ENV FLASK_RUN_HOST=0.0.0.0
+
+CMD ['gunicorn', "-b", "0.0.0.0:5000", "app:app"]
---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    message=None
+    if request.method == 'POST':
+        name = request.form['name']
+        message = f"Hello, {name}! Welcome to Flask"
+    return render_template("index.html", message=message)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
